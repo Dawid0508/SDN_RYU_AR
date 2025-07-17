0f89614d326dec09248096da9fa3907564f291ef
Dawid0508 <dawidgruszecki07@gmail.com>
2025-07-17 19:28:29 +0200

    new_chapter

---
+++ dijkstry_controller.py
@@ -0,0 +1,147 @@
+# Copyright (C) 2011 Nippon Telegraph and Telephone Corporation.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from ryu.base import app_manager
+from ryu.controller import ofp_event
+from ryu.controller.handler import CONFIG_DISPATCHER, MAIN_DISPATCHER
+from ryu.controller.handler import set_ev_cls
+from ryu.ofproto import ofproto_v1_3
+from ryu.lib.packet import packet
+from ryu.lib.packet import ethernet
+from ryu.lib.packet import ether_types
+from ryu.lib import_link
+from ryu.topology import event, switches
+import networkx as nx # <--- Użycie biblioteki networkx do operacji na grafach
+
+class SimpleSwitch13(app_manager.RyuApp):
+    OFP_VERSIONS = [ofproto_v1_3.OFP_VERSION]
+
+    def __init__(self, *args, **kwargs):
+        super(SimpleSwitch13, self).__init__(*args, **kwargs)
+        self.mac_to_port = {}
+        # <--- TUTAJ TWORZONY JEST GRAF SIECI
+        # DiGraph to graf skierowany, idealny do reprezentowania połączeń
+        self.net = nx.DiGraph() 
+        self.topology_api_app = self
+
+    @set_ev_cls(ofp_event.EventOFPSwitchFeatures, CONFIG_DISPATCHER)
+    def switch_features_handler(self, ev):
+        datapath = ev.msg.datapath
+        ofproto = datapath.ofproto
+        parser = datapath.ofproto_parser
+
+        # install table-miss flow entry
+        #
+        # We specify NO BUFFER to max_len of the output action due to
+        # OVS bug. At this moment, if we specify a max_len with valid
+        # buffer_id, controller will receive empty buffer_id and
+        # max_len still be 0.
+        match = parser.OFPMatch()
+        actions = [parser.OFPActionOutput(ofproto.OFPP_CONTROLLER,
+                                            ofproto.OFPCML_NO_BUFFER)]
+        self.add_flow(datapath, 0, match, actions)
+
+    def add_flow(self, datapath, priority, match, actions, buffer_id=None):
+        ofproto = datapath.ofproto
+        parser = datapath.ofproto_parser
+
+        inst = [parser.OFPInstructionActions(ofproto.OFPIT_APPLY_ACTIONS,
+                                                actions)]
+        if buffer_id:
+            mod = parser.OFPFlowMod(datapath=datapath, buffer_id=buffer_id,
+                                    priority=priority, match=match,
+                                    instructions=inst)
+        else:
+            mod = parser.OFPFlowMod(datapath=datapath, priority=priority,
+                                    match=match, instructions=inst)
+        datapath.send_msg(mod)
+
+    # <--- TUTAJ KONTROLER ODKRYWA TOPOLOGIĘ SIECI
+    # Ten dekorator sprawia, że funkcja jest wywoływana, gdy Ryu odkryje nowe połączenie
+    @set_ev_cls(event.EventSwitchEnter)
+    def get_topology_data(self, ev):
+        switch_list = get_switch(self.topology_api_app, None)
+        switches = [switch.dp.id for switch in switch_list]
+        self.net.add_nodes_from(switches)
+
+        links_list = get_link(self.topology_api_app, None)
+        links = [(link.src.dpid, link.dst.dpid, {'port': link.src.port_no}) for link in links_list]
+        self.net.add_edges_from(links)
+        links = [(link.dst.dpid, link.src.dpid, {'port': link.dst.port_no}) for link in links_list]
+        self.net.add_edges_from(links)
+
+    @set_ev_cls(ofp_event.EventOFPPacketIn, MAIN_DISPATCHER)
+    def _packet_in_handler(self, ev):
+        if ev.msg.msg_len < ev.msg.total_len:
+            self.logger.debug("packet truncated: only %s of %s bytes",
+                                ev.msg.msg_len, ev.msg.total_len)
+        msg = ev.msg
+        datapath = msg.datapath
+        ofproto = datapath.ofproto
+        parser = datapath.ofproto_parser
+        in_port = msg.match['in_port']
+
+        pkt = packet.Packet(msg.data)
+        eth = pkt.get_protocols(ethernet.ethernet)[0]
+
+        if eth.ethertype == ether_types.ETH_TYPE_LLDP:
+            # ignore lldp packet
+            return
+            
+        dst = eth.dst
+        src = eth.src
+        dpid = datapath.id
+        self.mac_to_port.setdefault(dpid, {})
+
+        # learn a mac address to avoid FLOOD next time.
+        self.mac_to_port[dpid][src] = in_port
+
+        if dst in self.mac_to_port[dpid]:
+            out_port = self.mac_to_port[dpid][dst]
+        else:
+            out_port = ofproto.OFPP_FLOOD
+
+        # <--- KLUCZOWY MOMENT DLA DIJKSTRY I FAMTAR
+        if src not in self.net:
+            self.net.add_node(src)
+            self.net.add_edge(dpid, src, port=in_port)
+            self.net.add_edge(src, dpid)
+        if dst in self.net:
+            # <--- WYWOŁANIE ALGORYTMU DIJKSTRY Z NETWORKX
+            # Funkcja shortest_path domyślnie użyje atrybutu 'weight' krawędzi do obliczeń.
+            # Jeśli wagi nie są jawnie zdefiniowane, przyjmuje wartość 1.
+            path = nx.shortest_path(self.net, src, dst)
+            next = path[path.index(dpid) + 1]
+            out_port = self.net[dpid][next]['port']
+            self.mac_to_port[dpid][dst] = out_port
+
+        actions = [parser.OFPActionOutput(out_port)]
+
+        # install a flow to avoid packet_in next time
+        if out_port != ofproto.OFPP_FLOOD:
+            match = parser.OFPMatch(in_port=in_port, eth_dst=dst, eth_src=src)
+            if msg.buffer_id != ofproto.OFP_NO_BUFFER:
+                self.add_flow(datapath, 1, match, actions, msg.buffer_id)
+                return
+            else:
+                self.add_flow(datapath, 1, match, actions)
+                
+        data = None
+        if msg.buffer_id == ofproto.OFP_NO_BUFFER:
+            data = msg.data
+
+        out = parser.OFPPacketOut(datapath=datapath, buffer_id=msg.buffer_id,
+                                    in_port=in_port, actions=actions, data=data)
+        datapath.send_msg(out)
---
+++ test_topo.py
@@ -0,0 +1,78 @@
+#!/usr/bin/python
+
+"""
+Skrypt tworzący topologię sieci do testowania kontrolera Ryu z algorytmem Dijkstry/FAMTAR.
+
+Topologia:
+    h1 --- s1 --- s2 --- s4 --- h4  (Ścieżka "szybka", przepustowość 100 Mbps)
+            |      |      |
+            \----- s3 -----/      (Ścieżka "wolna", przepustowość 10 Mbps)
+
+- h1, h4: Hosty końcowe
+- s1, s2, s3, s4: Przełączniki OpenFlow
+
+Aby uruchomić:
+1. W jednym terminalu uruchom kontroler Ryu:
+    ryu-manager dijkstra_Ryu_controller.py
+
+2. W drugim terminalu uruchom ten skrypt z uprawnieniami roota:
+    sudo python test_topo.py
+"""
+
+from mininet.net import Mininet
+from mininet.node import Controller, RemoteController, OVSKernelSwitch
+from mininet.cli import CLI
+from mininet.link import TCLink
+from mininet.log import setLogLevel, info
+
+def create_topology():
+    """Tworzy i uruchamia niestandardową topologię sieci."""
+
+    # Tworzenie obiektu sieci. Używamy RemoteController, aby połączyć się
+    # z kontrolerem Ryu działającym na tym samym komputerze.
+    # Używamy TCLink, aby móc definiować parametry połączeń (jak przepustowość).
+    net = Mininet(
+        controller=lambda name: RemoteController(name, ip='127.0.0.1', port=6653),
+        switch=OVSKernelSwitch,
+        link=TCLink,
+        autoSetMacs=True
+    )
+
+    info('*** Dodawanie hostów\n')
+    h1 = net.addHost('h1', ip='10.0.0.1/24')
+    h4 = net.addHost('h4', ip='10.0.0.4/24')
+
+    info('*** Dodawanie przełączników\n')
+    s1 = net.addSwitch('s1')
+    s2 = net.addSwitch('s2')
+    s3 = net.addSwitch('s3')
+    s4 = net.addSwitch('s4')
+
+    info('*** Tworzenie połączeń i ustawianie "kosztów" (przepustowości)\n')
+    
+    # Połączenia hostów z siecią
+    net.addLink(h1, s1)
+    net.addLink(h4, s4)
+
+    # ŚCIEŻKA SZYBKA (wysoka przepustowość = niski koszt dla algorytmu)
+    # Parametr bw (bandwidth) jest podany w Mb/s
+    net.addLink(s1, s2, bw=100)
+    net.addLink(s2, s4, bw=100)
+
+    # ŚCIEŻKA WOLNA (niska przepustowość = wysoki koszt dla algorytmu)
+    net.addLink(s1, s3, bw=10)
+    net.addLink(s3, s4, bw=10)
+
+    info('*** Uruchamianie sieci\n')
+    net.build()
+    net.start()
+
+    info('*** Uruchamianie interfejsu wiersza poleceń (CLI)\n')
+    CLI(net)
+
+    info('*** Zatrzymywanie sieci\n')
+    net.stop()
+
+if __name__ == '__main__':
+    setLogLevel('info')
+    create_topology()
