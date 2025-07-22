b3e2c6a0be1dcf262d0ba7aca288831b0ead5e6d
Dawid0508 <dawidgruszecki07@gmail.com>
2025-07-22 20:39:00 +0200

    v13

--- trying_fix.py
+++ trying_fix.py
@@ -2,12 +2,11 @@
 
 from ryu.base import app_manager
 from ryu.controller import ofp_event, dpset
-from ryu.controller.handler import CONFIG_DISPATCHER, MAIN_DISPATCHER, DEAD_DISPATCHER
+from ryu.controller.handler import CONFIG_DISPATCHER, MAIN_DISPATCHER
 from ryu.controller.handler import set_ev_cls
 from ryu.ofproto import ofproto_v1_3
 from ryu.lib.packet import packet, ethernet, ether_types
 from ryu.topology import event
-from ryu.topology.api import get_switch, get_link
 from ryu.lib import hub
 import networkx as nx
 
@@ -27,7 +26,7 @@ class FamtarController(app_manager.RyuApp):
         self.port_speed = {}
         self.link_to_port = {}
         self.congestion_state = {}
-        self.logger.info("--- Kontroler FAMTAR v1.2 (Final) uruchomiony ---")
+        self.logger.info("--- Kontroler FAMTAR v2.0 (Proaktywny) uruchomiony ---")
 
     def add_flow(self, datapath, priority, match, actions):
         ofproto = datapath.ofproto
@@ -37,13 +36,11 @@ class FamtarController(app_manager.RyuApp):
         datapath.send_msg(mod)
         self.logger.info(f"FLOW: Instaluję regułę na DPID {datapath.id:016x} dla {match} -> {actions}")
 
-    # --- Budowanie topologii i instalacja reguły table-miss ---
     @set_ev_cls(event.EventSwitchEnter)
     def switch_enter_handler(self, ev):
         datapath = ev.switch.dp
-        dpid = datapath.id
-        self.net.add_node(dpid)
-        self.logger.info(f"TOPOLOGY: Dodano przełącznik {dpid} do grafu.")
+        self.net.add_node(datapath.id)
+        self.logger.info(f"TOPOLOGY: Dodano przełącznik {datapath.id} do grafu.")
         
         ofproto = datapath.ofproto
         parser = datapath.ofproto_parser
@@ -51,12 +48,10 @@ class FamtarController(app_manager.RyuApp):
         actions = [parser.OFPActionOutput(ofproto.OFPP_CONTROLLER, ofproto.OFPCML_NO_BUFFER)]
         self.add_flow(datapath, 0, match, actions)
 
-    # WAŻNA ZMIANA: Usunięto MAIN_DISPATCHER, aby zapewnić odkrywanie połączeń
     @set_ev_cls(event.EventLinkAdd)
     def link_add_handler(self, ev):
-        link = ev.link
-        s1, s2 = link.src.dpid, link.dst.dpid
-        p1, p2 = link.src.port_no, link.dst.port_no
+        s1, s2 = ev.link.src.dpid, ev.link.dst.dpid
+        p1, p2 = ev.link.src.port_no, ev.link.dst.port_no
         self.net.add_edge(s1, s2, port=p1, weight=1)
         self.net.add_edge(s2, s1, port=p2, weight=1)
         self.link_to_port[(s1, s2)] = p1
@@ -65,18 +60,15 @@ class FamtarController(app_manager.RyuApp):
         self.congestion_state.setdefault((s2, s1), False)
         self.logger.info(f"TOPOLOGY: Dodano połączenie dwukierunkowe {s1} <-> {s2}")
 
-    # --- Mechanizm monitorowania FAMTAR ---
     def _monitor(self):
-        self.logger.info("MONITOR: Uruchomiono wątek monitorujący...")
         while True:
             for dp in self.dpset.get_all():
                 self._request_stats(dp[1])
             hub.sleep(5)
 
     def _request_stats(self, datapath):
-        ofproto = datapath.ofproto
         parser = datapath.ofproto_parser
-        req = parser.OFPPortStatsRequest(datapath, 0, ofproto.OFPP_ANY)
+        req = parser.OFPPortStatsRequest(datapath, 0, datapath.ofproto.OFPP_ANY)
         datapath.send_msg(req)
 
     @set_ev_cls(ofp_event.EventOFPPortStatsReply, MAIN_DISPATCHER)
@@ -84,12 +76,10 @@ class FamtarController(app_manager.RyuApp):
         body = ev.msg.body
         dpid = ev.msg.datapath.id
         for stat in sorted(body, key=lambda s: s.port_no):
-            port_no = stat.port_no
-            key = (dpid, port_no)
+            key = (dpid, stat.port_no)
             if key in self.port_stats:
                 prev_tx = self.port_stats[key]
-                delta_tx = stat.tx_bytes - prev_tx
-                speed_bps = (delta_tx * 8) / 5
+                speed_bps = ((stat.tx_bytes - prev_tx) * 8) / 5
                 self.port_speed[key] = speed_bps
             self.port_stats[key] = stat.tx_bytes
         self.update_link_costs()
@@ -97,27 +87,26 @@ class FamtarController(app_manager.RyuApp):
     def update_link_costs(self):
         for (s1, s2), port in self.link_to_port.items():
             key = (s1, port)
-            if key in self.port_speed:
-                speed_mbps = self.port_speed.get(key, 0) / (1000*1000)
-                link_capacity_mbps = 10 if 3 in (s1, s2) else 100
-                Th_max, Th_min = 0.9 * link_capacity_mbps, 0.7 * link_capacity_mbps
-                is_congested = self.congestion_state.get((s1, s2), False)
+            speed_mbps = self.port_speed.get(key, 0) / (1000*1000)
+            link_capacity_mbps = 10 if 3 in (s1, s2) else 100
+            Th_max, Th_min = 0.9 * link_capacity_mbps, 0.7 * link_capacity_mbps
+            is_congested = self.congestion_state.get((s1, s2), False)
 
-                if speed_mbps > Th_max and not is_congested:
-                    self.net[s1][s2]['weight'] = 1000
-                    self.congestion_state[(s1, s2)] = True
-                    self.logger.warning(f"FAMTAR: Przeciążenie na linku {s1}->{s2}! Koszt=1000. (Szybkość: {speed_mbps:.2f} Mbps)")
-                elif speed_mbps < Th_min and is_congested:
-                    self.net[s1][s2]['weight'] = 1
-                    self.congestion_state[(s1, s2)] = False
-                    self.logger.info(f"FAMTAR: Koniec przeciążenia na {s1}->{s2}. Koszt=1. (Szybkość: {speed_mbps:.2f} Mbps)")
+            if speed_mbps > Th_max and not is_congested:
+                self.net[s1][s2]['weight'] = 1000
+                self.congestion_state[(s1, s2)] = True
+                self.logger.warning(f"FAMTAR: Przeciążenie na linku {s1}->{s2}! Koszt=1000. (Szybkość: {speed_mbps:.2f} Mbps)")
+            elif speed_mbps < Th_min and is_congested:
+                self.net[s1][s2]['weight'] = 1
+                self.congestion_state[(s1, s2)] = False
+                self.logger.info(f"FAMTAR: Koniec przeciążenia na {s1}->{s2}. Koszt=1. (Szybkość: {speed_mbps:.2f} Mbps)")
 
-    # --- Logika routingu (PacketIn) ---
     @set_ev_cls(ofp_event.EventOFPPacketIn, MAIN_DISPATCHER)
     def _packet_in_handler(self, ev):
         msg = ev.msg
         datapath = msg.datapath
         dpid = datapath.id
+        parser = datapath.ofproto_parser
         pkt = packet.Packet(msg.data)
         eth = pkt.get_protocol(ethernet.ethernet)
 
@@ -131,31 +120,31 @@ class FamtarController(app_manager.RyuApp):
             dst_dpid, dst_port = self.mac_to_port[dst]
             try:
                 path = nx.shortest_path(self.net, dpid, dst_dpid, weight='weight')
+                
+                # --- PROAKTYWNA INSTALACJA - PONIŻSZA PĘTLA ROZWIĄZUJE PROBLEM Z PRĘDKOŚCIĄ ---
                 for i in range(len(path)):
                     current_dpid = path[i]
                     out_port = dst_port if i == len(path) - 1 else self.net.get_edge_data(current_dpid, path[i+1])['port']
                     dp = self.dpset.get(current_dpid)
                     if dp:
-                        match = dp.ofproto_parser.OFPMatch(eth_dst=dst)
-                        actions = [dp.ofproto_parser.OFPActionOutput(out_port)]
+                        match = parser.OFPMatch(eth_dst=dst)
+                        actions = [parser.OFPActionOutput(out_port)]
                         self.add_flow(dp, 1, match, actions)
                 
+                # Wyślij pierwszy pakiet, kolejne polecą już po zainstalowanych regułach
                 out_port_first_hop = dst_port if len(path) <= 1 else self.net.get_edge_data(dpid, path[1])['port']
-                actions = [datapath.ofproto_parser.OFPActionOutput(out_port_first_hop)]
-                self.send_packet_out(datapath, msg, actions)
+                actions = [parser.OFPActionOutput(out_port_first_hop)]
+                self._send_packet_out(datapath, msg, actions)
             except nx.NetworkXNoPath:
                 self.flood(msg)
         else:
             self.flood(msg)
 
     def flood(self, msg):
-        datapath = msg.datapath
-        ofproto = datapath.ofproto
-        parser = datapath.ofproto_parser
-        actions = [parser.OFPActionOutput(ofproto.OFPP_FLOOD)]
-        self.send_packet_out(datapath, msg, actions)
+        actions = [msg.datapath.ofproto_parser.OFPActionOutput(msg.datapath.ofproto.OFPP_FLOOD)]
+        self._send_packet_out(msg.datapath, msg, actions)
 
-    def send_packet_out(self, datapath, msg, actions):
+    def _send_packet_out(self, datapath, msg, actions):
         ofproto = datapath.ofproto
         parser = datapath.ofproto_parser
         out = parser.OFPPacketOut(datapath=datapath,
